28ab73fb5f512771344aadea54ab1878109e3475
ujjwalsht <121858754+ujjwalsht@users.noreply.github.com>
2025-03-31 08:19:56 +0545

    Delete Lab work 1 directory

--- Lab work 1/12/Rem.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-
-public interface Rem extends Remote {
-    public int addNum(int a, int b) throws RemoteException;
-    public int subNum(int a, int b) throws RemoteException;
-}
